dca521c6f796d33aed70797e363bef6473b79a9e
Gerardo Miranda <gerardodev25@gmail.com>
2026-05-04 19:03:02 -0400

    test: expand ProductsService unit tests with additional scenarios "02-nest-teslo-shop" - add test for BadRequestException on product creation failure - add test for findAll products with pagination - add test for findOne product with valid UUID - add test for NotFoundException when product not found - import required exceptions and DTOs

--- src/products/products.service.spec.ts
+++ src/products/products.service.spec.ts
@@ -5,6 +5,8 @@ import { Product, ProductImage } from './entities';
 import { DataSource, Repository } from 'typeorm';
 import { User } from '../auth/entities/user.entity';
 import { CreateProductDto } from './dto/create-product.dto';
+import { BadRequestException, NotFoundException } from '@nestjs/common';
+import { PaginationDto } from 'src/common/dtos/pagination.dto';
 
 describe('ProductsService', () => {
   let service: ProductsService;
@@ -100,5 +102,91 @@ describe('ProductsService', () => {
       images: ['image1.jpg'],
       price: 100,
     });
+  });
+
+  it('should throw a BadRequestException if create product fails', async () => {
+    const dto = {
+      title: 'Test Product',
+      images: ['image1.jpg'],
+      price: 100,
+    } as CreateProductDto;
+
+    const user = {
+      id: '1',
+      email: 'test@example.com',
+    } as User;
+
+    const error = {
+      code: '23505',
+      detail: 'mock error details',
+    };
+    jest.spyOn(productRepository, 'save').mockRejectedValue(error);
+
+    await expect(service.create(dto, user)).rejects.toThrow(error.detail);
+    await expect(service.create(dto, user)).rejects.toThrow(
+      BadRequestException,
+    );
+  });
+
+  it('should find all products', async () => {
+    const paginationDto: PaginationDto = {
+      limit: 10,
+      offset: 0,
+      gender: 'kid',
+    };
+
+    const products = [
+      {
+        id: '1',
+        title: 'Test Product',
+        images: [{ id: 1, url: 'image1.jpg', product: {} as Product }],
+      },
+      {
+        id: '2',
+        title: 'Test Product',
+        images: [{ id: 2, url: 'image2.jpg', product: {} as Product }],
+      },
+    ] as Product[];
+
+    const count = products.length;
+    jest.spyOn(productRepository, 'find').mockResolvedValue(products);
+    jest.spyOn(productRepository, 'count').mockResolvedValue(count);
+
+    const result = await service.findAll(paginationDto);
+
+    expect(result).toEqual({
+      count,
+      pages: 1,
+      products: products.map((p) => ({
+        ...p,
+        images: p.images.map((img) => img.url),
+      })),
+    });
+  });
+
+  it('should find a product bt valid uuid', async () => {
+    const productId = '123e4567-e89b-12d3-a456-426614174000';
+
+    const product = {
+      id: productId,
+      title: 'Test Product',
+    } as Product;
+
+    jest.spyOn(productRepository, 'findOneBy').mockResolvedValue(product);
+
+    const result = await service.findOne(productId);
+
+    expect(result).toEqual(product);
+  });
+
+  it('should throw and error if id not found', async () => {
+    const productId = '123e4567-e89b-12d3-a456-426614174000';
+
+    jest.spyOn(productRepository, 'findOneBy').mockResolvedValue(null);
+
+    await expect(service.findOne(productId)).rejects.toThrow(NotFoundException);
+    await expect(service.findOne(productId)).rejects.toThrow(
+      `Product with ${productId} not found`,
+    );
   });
 });
