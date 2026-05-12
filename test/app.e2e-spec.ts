9e8b55f69e3daa8361a51fa8856cea3a0a07b8da
Gerardo Miranda <gerardodev25@gmail.com>
2026-05-11 21:55:39 -0400

    feat(e2e): add authentication e2e tests and update configuration
    - Add login e2e test in test/e2e/auth/login.e2e-spec.ts
    - Update jest-e2e.json configuration for e2e testing
    - Update package.json with e2e test scripts
    - Update test/app.e2e-spec.ts configuration

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,7 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
+import { AppModule } from '../src/app.module';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
@@ -20,11 +20,6 @@ describe('AppController (e2e)', () => {
   });
 
   it('/ (GET)', () => {
-    // return request(app.getHttpServer())
-    //   .get('/')
-    //   .expect(200)
-    //   .expect('Hello World!');
-
-    expect(true).toBeTruthy();
+    return request(app.getHttpServer()).get('/').expect(404);
   });
 });
